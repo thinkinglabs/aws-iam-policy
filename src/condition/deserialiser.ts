e30a87f534f9f3fcb8580923f1c06e04633c8652
Thierry de Pauw <thierry@thinkinglabs.io>
2021-05-15 14:44:20 +0200

    Add deserialisation of a Condition JSON object having one operator, one key and one value (#1)

--- src/condition/deserialiser.ts
+++ src/condition/deserialiser.ts
@@ -4,6 +4,12 @@ export class ConditionJSONDeserialiser {
   static fromJSON(
       input: {[condition:string]: {[key:string]: string[]}} | undefined,
   ): Condition[] {
-    return [];
+    if (input === undefined) {
+      return [];
+    }
+    const operator = Object.keys(input)[0];
+    const key = Object.keys(input[operator])[0];
+    const values = input[operator][key];
+    return [new Condition(operator, key, values)];
   };
 }
--- tests/condition/deserialiser.spec.ts
+++ tests/condition/deserialiser.spec.ts
@@ -1,11 +1,23 @@
 import {expect} from 'chai';
+import {Condition} from '../../src/condition/condition';
 import {ConditionJSONDeserialiser} from '../../src/condition/deserialiser';
 
 describe('#ConditionJSONDeserialiser', function() {
   describe('#fromJSON', function() {
     describe('when having an undefined condition', function() {
       it('should return an empty array', function() {
-        expect(ConditionJSONDeserialiser.fromJSON(undefined)).to.deep.equal([]);
+        const input = undefined;
+        const expected: Condition[] = [];
+        expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
+      });
+    });
+    describe('when having a condition with one operator, one key and one value', function() {
+      it('should return one condition', function() {
+        const input = {
+          'operator': {'key': ['value']},
+        };
+        const expected = [new Condition('operator', 'key', ['value'])];
+        expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
   });
