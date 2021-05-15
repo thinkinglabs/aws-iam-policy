ae17908a8534a27df2c942dbc3a67ad8468ea6f7
Thierry de Pauw <thierry@thinkinglabs.io>
2021-05-15 13:17:07 +0200

    Extract merge methods for ConditionJSONSerialiser.toJSON() (#1)

--- src/condition/serialiser.ts
+++ src/condition/serialiser.ts
@@ -5,23 +5,47 @@ export class ConditionJSONSerialiser {
     if (conditions.length == 0) {
       return undefined;
     }
-    const result: { [test: string]: {[key: string]: string[]}} = {};
-    conditions.forEach((condition) => {
-      const obj = condition.toJSON();
-      Object.keys(obj).forEach((key) => {
-        if (key in result) {
-          Object.keys(obj[key]).forEach((subKey) => {
-            if (subKey in result[key]) {
-              result[key][subKey].push(...obj[key][subKey]);
-            } else {
-              result[key][subKey] = obj[key][subKey];
-            }
-          });
-        } else {
-          result[key] = obj[key];
-        }
+    return merge(conditions);
+
+    function merge(conditions: Condition[]) {
+      const result: { [test: string]: {[key: string]: string[]}} = {};
+
+      conditions.forEach((condition) => {
+        const json = condition.toJSON();
+
+        Object.keys(json).forEach((test) => {
+          mergeTest(result, json, test);
+        });
       });
-    });
-    return result;
+      return result;
+    }
+
+    function mergeTest(
+        result: {[test: string]: {[key: string]: string[]}},
+        json: {[test: string]: {[key: string]: string[]}},
+        test: string,
+    ) {
+      const jsonTest = json[test];
+      if (test in result) {
+        Object.keys(jsonTest).forEach((key) => {
+          mergeKey(result[test], jsonTest, key);
+        });
+      } else {
+        result[test] = jsonTest;
+      }
+    }
+
+    function mergeKey(
+        resultTest: {[key:string]: string[]},
+        jsonTest: {[key:string]: string[]},
+        key: string,
+    ) {
+      const values = jsonTest[key];
+      if (key in resultTest) {
+        resultTest[key].push(...values);
+      } else {
+        resultTest[key] = values;
+      }
+    }
   }
 }
