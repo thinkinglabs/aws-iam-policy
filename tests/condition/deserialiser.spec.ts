b6e47330eaa02b5c44404c163b1f438004d18f30
Thierry de Pauw <thierry@thinkinglabs.io>
2021-05-15 18:33:43 +0200

    Rename the ConditionJSONDeserialiser specs (#1)

--- tests/condition/deserialiser.spec.ts
+++ tests/condition/deserialiser.spec.ts
@@ -4,14 +4,14 @@ import {ConditionJSONDeserialiser} from '../../src/condition/deserialiser';
 
 describe('#ConditionJSONDeserialiser', function() {
   describe('#fromJSON', function() {
-    describe('when having an undefined Condition', function() {
+    describe('when Condition is undefined', function() {
       it('should return an empty array', function() {
         const input = undefined;
         const expected: Condition[] = [];
         expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
-    describe('when having a Condition with one operator, one key and one value', function() {
+    describe('when Condition has one operator, one key and one value', function() {
       it('should return one Condition', function() {
         const input = {
           'operator': {'key': ['value']},
@@ -20,7 +20,7 @@ describe('#ConditionJSONDeserialiser', function() {
         expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
-    describe('when having a Condition with one operator, one key and two values', function() {
+    describe('when Condition has one operator, one key and two values', function() {
       it('should return one Condition', function() {
         const input = {
           'operator': {'key': ['value1', 'value2']},
@@ -29,7 +29,7 @@ describe('#ConditionJSONDeserialiser', function() {
         expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
-    describe('when having a Condition with one operator, two keys each with one value', function() {
+    describe('when Condition has one operator, two keys each with one value', function() {
       it('should return two conditions', function() {
         const input = {
           'operator': {'key1': ['value1'], 'key2': ['value2']},
@@ -41,7 +41,7 @@ describe('#ConditionJSONDeserialiser', function() {
         expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
-    describe('when having a Condition with two operators each with one key and one value', function() {
+    describe('when Condition has two operators each with one key and one value', function() {
       it('should return two conditions', function() {
         const input = {
           'operator1': {'key1': ['value1']},
@@ -54,7 +54,7 @@ describe('#ConditionJSONDeserialiser', function() {
         expect(ConditionJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
       });
     });
-    describe('when having a Condition with two operators each with two keys and two values', function() {
+    describe('when Condition has two operators each with two keys and two values', function() {
       it('should return two conditions', function() {
         const input = {
           'operator1': {'key11': ['value111', 'value112'], 'key12': ['value121', 'value122']},
