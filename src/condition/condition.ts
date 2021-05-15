a2340aa1bc5463be55c240907299e458bd869ecb
Thierry de Pauw <thierry@thinkinglabs.io>
2021-05-15 07:55:39 +0200

    Add validation on Condition to check for an empty test

--- src/condition/condition.ts
+++ src/condition/condition.ts
@@ -1,10 +1,13 @@
 
 export class Condition {
-  public operator: string;
-  public key: string;
-  public values: string[];
+  public readonly operator: string;
+  public readonly key: string;
+  public readonly values: string[];
 
   constructor(test: string, variable: string, ...values: string[]) {
+    if (test === '') {
+      throw new Error('Empty test');
+    }
     this.operator = test;
     this.key = variable;
     this.values = values;
--- tests/condition/condition.spec.ts
+++ tests/condition/condition.spec.ts
@@ -1,2 +1,13 @@
-// import {expect} from 'chai';
+import {expect} from 'chai';
+import {Condition} from '../../src/condition/condition';
 
+describe('#Condition', function() {
+  describe('constructor', function() {
+    describe('when test is empty', function() {
+      it('should raise an error', function() {
+        expect(() => new Condition('', 'key', 'value')).to
+            .throw(Error).with.property('message', 'Empty test');
+      });
+    });
+  });
+});
