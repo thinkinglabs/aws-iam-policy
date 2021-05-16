075ff8ccf8988894195f33f41f0a4f9821dd7f8f
Thierry de Pauw <thierry@thinkinglabs.io>
2021-05-16 11:17:13 +0200

    Remove the rest parameter syntax for Statement.add### (#1)

--- src/statement/statement.ts
+++ src/statement/statement.ts
@@ -19,25 +19,25 @@ export class Statement {
     this.sid = props?.sid;
     this.effect = props?.effect || 'Allow';
 
-    this.addPrincipals(...props?.principals || []);
-    this.addActions(...props?.actions || []);
-    this.addResources(...props?.resources || []);
-    this.addConditions(...props?.conditions || []);
+    this.addPrincipals(props?.principals || []);
+    this.addActions(props?.actions || []);
+    this.addResources(props?.resources || []);
+    this.addConditions(props?.conditions || []);
   }
 
-  private addPrincipals(...principals: Principal[]) {
+  private addPrincipals(principals: Principal[]) {
     this.principals.push(...principals);
   }
 
-  private addActions(...actions: string[]) {
+  private addActions(actions: string[]) {
     this.actions.push(...actions);
   };
 
-  private addResources(...resources: string[]) {
+  private addResources(resources: string[]) {
     this.resources.push(...resources);
   };
 
-  private addConditions(...conditions: Condition[]) {
+  private addConditions(conditions: Condition[]) {
     this.conditions.push(...conditions);
   }
 
