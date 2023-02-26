1b5ea6e42986ae98219fa82a06036191465d9916
Thierry de Pauw <thierry@thinkinglabs.io>
2023-02-26 22:05:19 +0100

    Extend RootAccountPrincipal tests with a toJSON test (#16)

--- tests/principals/root-account.spec.ts
+++ tests/principals/root-account.spec.ts
@@ -2,17 +2,23 @@ import {expect} from 'chai';
 import {RootAccountPrincipal} from '../../src/principals/root-account';
 
 describe('#RootAccountPrincipal', function() {
+  describe('#toJSON', function() {
+    const account = '123456789000';
+    const principal = new RootAccountPrincipal(account);
+
+    it('should return the AWS principal JSON fragment', function() {
+      const expected = {
+        'AWS': 'arn:aws:iam::123456789000:root',
+      };
+      expect(principal.toJSON()).to.deep.equal(expected);
+    });
+  });
+
   describe('#validate', function() {
     describe('when given a valid root account arn', function() {
       it('should return the AWS account arn', function() {
         const arn = 'arn:aws:iam::012345678900:root';
         expect(RootAccountPrincipal.validate(arn)).to.equal('arn:aws:iam::012345678900:root');
-      });
-    });
-
-    describe('when given an invalid arn', function() {
-      it('should return undefined', function() {
-        expect(RootAccountPrincipal.validate('anARN')).to.be.undefined;
       });
     });
 
@@ -24,7 +30,13 @@ describe('#RootAccountPrincipal', function() {
 
     describe('when given a valid IAM Role arn', function() {
       it('should return undefined', function() {
-        expect(RootAccountPrincipal.validate('arn:aws:iam::012345678900:role/aUser')).to.be.undefined;
+        expect(RootAccountPrincipal.validate('arn:aws:iam::012345678900:role/aRole')).to.be.undefined;
+      });
+    });
+
+    describe('when given an invalid arn', function() {
+      it('should return undefined', function() {
+        expect(RootAccountPrincipal.validate('anARN')).to.be.undefined;
       });
     });
   });
