427f70741151123812b6c52bdf3860a0518219b7
Thierry de Pauw <thierry@thinkinglabs.io>
2023-11-20 21:40:04 +0100

    Revert "Add User principal (#16)"
    This reverts commit 64b5564ff6216a0c02cc1000f6b457cb72fb9191.

--- tests/principals/deserialiser.spec.ts
+++ tests/principals/deserialiser.spec.ts
@@ -6,7 +6,6 @@ import {RootAccountPrincipal} from '../../src/principals/root-account';
 import {AccountPrincipal} from '../../src/principals/account';
 import {AnonymousUserPrincipal} from '../../src/principals/anonymous';
 import {FederatedPrincipal} from '../../src/principals/federated';
-import {UserPrincipal} from '../../src/principals/user';
 
 describe('#PrincipalJSONDeserialise', function() {
   describe('#fromJSON', function() {
@@ -25,79 +24,32 @@ describe('#PrincipalJSONDeserialise', function() {
       });
     });
 
-    describe('when having one AWS principal', function() {
-      describe('as a single string', function() {
-        describe('with an unsupported arn', function() {
-          it('should throw an error', function() {
-            expect(() => PrincipalJSONDeserialiser.fromJSON({AWS: 'anArn'})).to.throw(Error)
-                .with.property('message', 'Unsupported AWS principal value "anArn"');
-          });
-        });
-
-        describe('with an anonymous user principal', function() {
-          it('should return one AnonymousUserPrincipal', function() {
-            const input = {AWS: '*'};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new AnonymousUserPrincipal()]);
-          });
-        });
-
-        describe('with an IAM user arn', function() {
-          it('should return one UserPrincipal', function() {
-            const arn = 'arn:aws:iam::012345678900:user/aUser';
-            const input = {AWS: arn};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([
-              new UserPrincipal('012345678900', 'aUser'),
-            ]);
-          });
-        });
-
-        describe('with one arn', function() {
-          it('should return one ArnPrincipal', function() {
-            const arn = 'arn:aws:iam::012345678900:role/aRole';
-            const input = {AWS: arn};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new ArnPrincipal(arn)]);
-          });
+    describe('when having an AWS principal', function() {
+      describe('with one ARN as 1-length array', function() {
+        it('should return one ArnPrincipal', function() {
+          const arn = 'arn:aws:iam::012345678900:user/aUser';
+          const input = {AWS: [arn]};
+          expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new ArnPrincipal(arn)]);
         });
       });
 
-      describe('as a 1-length array', function() {
-        describe('with an unsupported arn', function() {
-          it('should throw an error', function() {
-            expect(() => PrincipalJSONDeserialiser.fromJSON({AWS: ['anArn']})).to.throw(Error)
-                .with.property('message', 'Unsupported AWS principal value "anArn"');
-          });
-        });
-
-        describe('with an anonymous user principal', function() {
-          it('should return one AnonymousUserPrincipal', function() {
-            const input = {AWS: ['*']};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new AnonymousUserPrincipal()]);
-          });
-        });
-
-        describe('with an IAM user arn', function() {
-          it('should return one UserPrincipal', function() {
-            const arn = 'arn:aws:iam::012345678900:user/aUser';
-            const input = {AWS: [arn]};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([
-              new UserPrincipal('012345678900', 'aUser'),
-            ]);
-          });
-        });
-
-        describe('with one arn', function() {
-          it('should return one ArnPrincipal', function() {
-            const arn = 'arn:aws:iam::012345678900:role/aRole';
-            const input = {AWS: [arn]};
-            expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new ArnPrincipal(arn)]);
-          });
+      describe('with one ARN as a single string', function() {
+        it('should return one ArnPrincipal', function() {
+          const arn = 'arn:aws:iam::012345678900:user/aUser';
+          const input = {AWS: arn};
+          expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new ArnPrincipal(arn)]);
         });
       });
-    });
 
-    describe('when having two AWS principals', function() {
-      describe('with two IAM roles', function() {
-        it('should return a list of two ArnPrincipal', function() {
+      describe('with an anonymous user principal', function() {
+        it('should return one AnonymousUserPrincipal', function() {
+          const input = {AWS: ['*']};
+          expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal([new AnonymousUserPrincipal()]);
+        });
+      });
+
+      describe('with two ARNs', function() {
+        it('should return a list having two ArnPrincipal', function() {
           const role1Arn = 'arn:aws:iam::111122223333:role/role1';
           const role2Arn = 'arn:aws:iam::111122223333:role/role2';
           const input = {AWS: [role1Arn, role2Arn]};
@@ -105,11 +57,9 @@ describe('#PrincipalJSONDeserialise', function() {
           expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
         });
       });
-    });
 
-    describe('when having three AWS principals', function() {
       describe('with an IAM user, a root user and an account ID', function() {
-        it('should return a list having UserPrincipal, RootAccountPrincipal and AccountPrincipal', function() {
+        it('should return a list having an ArnPrincipal and a ServicePrincipal', function() {
           const accountID = '012345678900';
           const userArn = `arn:aws:iam::${accountID}:user/aUser`;
           const rootAccountArn = `arn:aws:iam::${accountID}:root`;
@@ -120,6 +70,13 @@ describe('#PrincipalJSONDeserialise', function() {
             new AccountPrincipal(accountID),
           ];
           expect(PrincipalJSONDeserialiser.fromJSON(input)).to.deep.equal(expected);
+        });
+      });
+
+      describe('with an unsupported ARN', function() {
+        it('should throw an error', function() {
+          expect(() => PrincipalJSONDeserialiser.fromJSON({AWS: ['anArn']})).to.throw(Error)
+              .with.property('message', 'Unsupported AWS principal value "anArn"');
         });
       });
     });
