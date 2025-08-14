b2c1fd4abaa636d8b87365249d20e2592ada770c
Thierry de Pauw <thierry@thinkinglabs.io>
2025-08-15 00:09:51 +0200

    Validate a list of actions (#31)

--- tests/statement/actions.spec.ts
+++ tests/statement/actions.spec.ts
@@ -13,4 +13,31 @@ describe('#Action', () => {
   it('should validate iam:Create????', () => {
     expect(validate(['iam:Create????'])).to.be.empty;
   });
+
+  it('should validate an array of valid actions', () => {
+    expect(validate(['iam:CreateUser', 'iam:DeleteUser'])).to.be.empty;
+  });
+
+  it('should validate an array of valid actions with wildcards', () => {
+    expect(validate([
+      'iam:Create*',
+      'iam:Delete*',
+      'iam:UpdateRol?',
+    ])).to.be.empty;
+  });
+
+  it('should not validate an array with invalid actions', () => {
+    expect(validate([
+      'iam:CreateRole',
+      'iam:Delete?',
+      'iam:Describe*',
+      'iam:DescribeUser',
+    ])).to.be.deep.equal([
+      'Invalid action: iam:Delete?',
+      'Invalid action: iam:Describe*',
+      'Invalid action: iam:DescribeUser',
+    ]);
+  });
+
+
 });
