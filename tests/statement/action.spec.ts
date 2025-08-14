04fc19be82f6f4408d7fbcaa873cd7918023ae64
Thierry de Pauw <thierry@thinkinglabs.io>
2025-08-14 23:27:56 +0200

    Fails to validate iam:Create? (#31)

--- tests/statement/action.spec.ts
+++ tests/statement/action.spec.ts
@@ -23,4 +23,27 @@ describe('#Action', () => {
     );
     expect(actual).to.be.true;
   });
+
+  it('should not validate iam:Create?', () => {
+    const action = 'iam:Create?';
+    const validIamActions = [
+      'iam:CreateUser',
+      'iam:DeleteUser',
+      'iam:UpdateUser',
+      'iam:CreateGroup',
+      'iam:DeleteGroup',
+      'iam:UpdateGroup',
+      'iam:CreatePolicy',
+      'iam:DeletePolicy',
+      'iam:UpdatePolicy',
+      'iam:CreateRole',
+      'iam:DeleteRole',
+      'iam:UpdateRole',
+    ];
+    const actual = validIamActions.reduce(
+        (accumulator, currentValue) => accumulator || new RegExp(`^${action.replaceAll('?', '.')}$`).test(currentValue),
+        false,
+    );
+    expect(actual).to.be.false;
+  });
 });
